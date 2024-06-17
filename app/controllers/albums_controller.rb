1560a879bbbd27eb0d4489731f654e7c159ab01f
Haluk Can Sarıöz <76582367+halukcansarioz@users.noreply.github.com>
2024-06-17 12:49:09 +0300

    Step 5: Adding images of albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,30 +1,18 @@
 class AlbumsController < ApplicationController
   def show
     @album = fetch_album(params[:user_id], params[:id])
-    if @album
-      @photos = fetch_photos(@album['id'])
-    else
-      redirect_to root_path, alert: "Albüm bulunamadı."
-    end
+    @photos = fetch_photos(@album['id'])
   end
 
   private
 
   def fetch_album(user_id, album_id)
     response = HTTParty.get("https://jsonplaceholder.typicode.com/albums?userId=#{user_id}")
-    if response.success?
-      JSON.parse(response.body).find { |album| album['id'] == album_id.to_i }
-    else
-      nil
-    end
+    JSON.parse(response.body).find { |album| album['id'] == album_id.to_i }
   end
 
   def fetch_photos(album_id)
     response = HTTParty.get("https://jsonplaceholder.typicode.com/photos?albumId=#{album_id}")
-    if response.success?
-      JSON.parse(response.body)
-    else
-      []
-    end
+    JSON.parse(response.body)
   end
 end
